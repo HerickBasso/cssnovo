4bfc22a866960a8890fc8223737d47f32b04ab0a
Lodzin01 <herickbasso@rede.ulbra.br>
2023-04-25 14:58:16 -0300

    first commit

---
+++ cadastro.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/cad.css">
+    <title>Document</title>
+</head>
+<body>
+
+<h1> Tela de Cadastro </h1>
+
+<form action="">
+
+<div class="email">
+<input type="email" placeholder="email"  required >
+</div>
+
+<div class="senha">
+<input type="password" plaholder="senha" required >
+</div>
+
+
+</form>
+
+<div class="cadastrar">
+    <input type="submit">
+</div>
+
+<div class="cancel">
+
+<button>Cancelar</button>
+
+</div>
+
+</body>
+</html>
---
+++ formrecado.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/recado.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1> Adicione o Recado </h1>
+
+<div class="txt">
+
+<textarea name="" id="" cols="30" rows="10"></textarea>
+
+</div>
+
+<div class="data">
+<input type="date">
+</div>
+
+<div class="salvar">
+<input type="submit">
+</div>
+
+<div class="btn">
+<button>Cancelar</button>
+</div>
+
+
+</body>
+</html>
